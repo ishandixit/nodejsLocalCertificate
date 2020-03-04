d5e9dbb9891df676139d551f1b225d2aa6b53fd8
Ishan Bhardwaj <ishan.bhardwaj@publicissapient.com>
2020-03-04 18:08:47 +0530

    apple

--- src/App.js
+++ src/App.js
@@ -3,25 +3,38 @@ import React from 'react';
 import './App.css';
 
 class App extends React.Component {
-  componentDidMount() {
+  async componentDidMount() {
+    const buttonElement = document.getElementById('sign-in-with-apple-button');
     
+      const script = document.createElement("script");
+
+      script.src = "https://appleid.cdn-apple.com/appleauth/static/jsapi/appleid/1/en_US/appleid.auth.js";
+      script.async = true;
+
+      document.body.appendChild(script);
+      buttonElement.addEventListener('click', async(data,data1) => {
+        debugger;
+     
+  //     console.log(res,"sdfjbsfjbdsfkjskf")
+      
+    });
     console.log("hrllo  ")
-    document.addEventListener('AppleIDSignInOnSuccess', (data) => {
+    buttonElement.addEventListener('AppleIDSignInOnSuccess', (data) => {
       debugger;
          //handle successful response
          console.log(data,"success")
     });
     //Listen for authorization failures
-    document.addEventListener('AppleIDSignInOnFailure', (error) => {
+    buttonElement.addEventListener('AppleIDSignInOnFailure', (error) => {
          //handle error.
          console.log("error ",error)
     })}
  render(){
   return (
     <div className="App">
-              <div id="appleid-signin" data-color="black" data-border="true" data-type="sign in">signin</div>
+              <div id="sign-in-with-apple-button" data-color="black" data-border="true" data-type="sign in">signin</div>
 
-     
+           
     </div>
   );}
 }
